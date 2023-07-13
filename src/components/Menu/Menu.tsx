174c3d33eb9bd82aaf4216a03a3e2f24b744d33a
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-07-13 12:09:47 +0530

    minor changes

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
 import "./menu.scss";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/">
+          <img src="/home.svg" alt="" />
+          <span className="listItemTitle">Home</span>
+        </Link>
+        <Link to="/">
+          <img src="/profile.svg" alt="" />
+          <span className="listItemTitle">Profile</span>
+        </Link>
+      </div>
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/">
+          <img src="/home.svg" alt="" />
+          <span className="listItemTitle">Home</span>
+        </Link>
+        <Link to="/">
+          <img src="/profile.svg" alt="" />
+          <span className="listItemTitle">Profile</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,31 @@
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>SuperB.</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://images.unsplash.com/photo-1534030347209-467a5b0ad3e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+            alt=""
+          />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
