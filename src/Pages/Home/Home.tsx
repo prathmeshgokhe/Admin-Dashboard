b23da8ddebc4c74145b084fc8d4ca3ccada35ac7
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-07-18 11:33:32 +0530

    ui fix

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -25,17 +25,17 @@ const Home = () => {
       <div className="box box3">
         <ChartBox {...chartBoxProduct} />
       </div>
-      <div className="box box4">
-        <PieChartbox />
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
       </div>
       <div className="box box5">
         <ChartBox {...chartBoxConversion} />
       </div>
-      <div className="box box6">
-        <ChartBox {...chartBoxRevenue} />
-      </div>
       <div className="box box7">
         <BigChartBox />
+      </div>
+      <div className="box box4">
+        <PieChartbox />
       </div>
       <div className="box box8">
         <BarChartBox {...barChartBoxRevenue} />
